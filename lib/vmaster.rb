d0b8e0a62df2820b41a6f77199bce3bb1e6f3708
Radim Marek <radim@laststation.net>
2012-04-06 13:13:17 +0200

    Implicitly call Commander::Runner.run! instead of relying on at_exit { run! } initialization

--- lib/vmaster.rb
+++ lib/vmaster.rb
@@ -1,8 +1,23 @@
+
+require 'commander/delegates'
 require "vmaster/version"
 require "vmaster/helpers"
 require "vmaster/cli"
-require "commander/import"
-require "vmaster/request.rb"
+require "vmaster/request"
+
+require 'commander'
+require 'commander/delegates'
+
+#
+# snippet from commander:lib/commander/import.rb needed when manually calling
+# run! instead of relying on at_exit { run! } (sic!).
+#
+include Commander::UI
+include Commander::UI::AskForClass
+include Commander::Delegates
+
+$terminal.wrap_at = HighLine::SystemExtensions.terminal_size.first - 5 rescue 80 if $stdin.tty?
+
 
 # include commands
 require 'vmaster/config_command'
