375d19920192987416e65786e8ba76db4659eb65
Radim Marek <radim@laststation.net>
2012-05-24 13:06:33 +0200

    User can specify alternative config file
    Usage: virtualmaster command --config CONFIG_FILE

--- lib/vmaster/cli.rb
+++ lib/vmaster/cli.rb
@@ -3,41 +3,52 @@ require 'deltacloud'
 
 module VirtualMaster
   class CLI
+    @@config_file = nil
     @@api = nil
     @@config = nil
     @@callbacks = []
 
     def self.run
-      # load config
-      config_file = File.join(ENV["HOME"], ".virtualmaster")
-      if File.exists? config_file
-        config = YAML::load(File.open(config_file))
-
-        @@config = config.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-
-        unless @@config[:profiles]
-          puts "WARNING: Please, reconfigure virtualmaster-cli (delete your '~/.virtualmaster' and run `virtualmaster config` again). This way you can use custom instance profiles correctly (new in version 0.0.5)"
-          puts
-        end
-
-        begin
-          @@api = DeltaCloud.new(@@config[:username], @@config[:password], VirtualMaster::DEFAULT_URL)
-        rescue DeltaCloud::API::BackendError => e
-          abort "Invalid API response: #{e.message}"
-        rescue Exception => e
-          abort "Unable to connect to Virtualmaster's DeltaCloud API: #{e.message}" 
-        end
-      end
+      @@config_file = File.join(ENV["HOME"], ".virtualmaster")
 
       yield
     end
 
+    def self.config_file(config)
+      @@config_file = config
+    end
+
     def self.api
+      self.config unless @@config
+
       abort "No configuration available! Please run 'virtualmaster config' first!" unless @@api
       @@api
     end
 
     def self.config
+      unless @@config
+        if File.exists? @@config_file
+          config = YAML::load(File.open(@@config_file))
+
+          @@config = config.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+          unless @@config[:profiles]
+            puts "WARNING: Please, reconfigure virtualmaster-cli (delete your '~/.virtualmaster' and run `virtualmaster config` again). This way you can use custom instance profiles correctly (new in version 0.0.5)"
+            puts
+          end
+
+          begin
+            @@api = DeltaCloud.new(@@config[:username], @@config[:password], VirtualMaster::DEFAULT_URL)
+          rescue DeltaCloud::API::BackendError => e
+            abort "Invalid API response: #{e.message}"
+          rescue Exception => e
+            abort "Unable to connect to Virtualmaster's DeltaCloud API: #{e.message}" 
+          end
+        else 
+          abort "Specified configuration file (#{config_file}) does not exist!"
+        end
+      end
+
       @@config
     end
 
